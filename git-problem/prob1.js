ea09bff4180011cc1bf17fc7b6c28ce95a6ef045
gopal <rk7544827919@gamil.com>
2022-09-13 19:05:23 +0530

    prob1 and prob2

---
+++ git-problem/prob1.js
@@ -0,0 +1,12 @@
+
+// Check Palindrome
+    let str=nurupull;
+    let new_s="";
+    for (let i=N-1;i>=0;i++){
+        new_s=new_s+str[i];
+    }
+    if (str==new_s){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
---
+++ git-problem/prob2.js
@@ -0,0 +1,11 @@
+let num=13;
+for(var i=1;i<=num;i++){
+    if (num%i===0){
+          prime="Yes";
+    }else{
+        prime="No";
+    }
+ 
+    }
+ console.log(prime);
+}
